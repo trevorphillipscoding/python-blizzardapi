81653a2456ca2153129698f44c7a9b8c3f8608ef
Trevor Phillips <trevorcoreyphillips@gmail.com>
2020-11-02 21:41:19 -0700

    updated fixtures

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -9,28 +9,6 @@ def get_success_response():
     return mock
 
 
-def get_exception_response():
-    mock = requests.exceptions.RequestException()
-    return mock
-
-
-def get_failed_response():
-    mock = requests.models.Response()
-    mock.status_code = 401
-    mock._content = b"{}"
-    return mock
-
-
 @pytest.fixture
 def success_response_mock(mocker):
     return mocker.patch("requests.Session.get", return_value=get_success_response())
-
-
-@pytest.fixture
-def exception_response_mock(mocker):
-    return mocker.patch("requests.Session.get", return_value=get_exception_response())
-
-
-@pytest.fixture
-def failed_response_mock(mocker):
-    return mocker.patch("requests.Session.get", return_value=get_failed_response())
